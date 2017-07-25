329940f8076a9c79bd9bf777d4147004691ea505
raj <raj@rajs-MacBook-Pro.local>
2017-07-25 14:40:13 -0400

    added in async controller for ingestion process and changed start script accordingly.

--- startScript.sh
+++ startScript.sh
@@ -3,8 +3,16 @@
 
  docker-compose up -d
 
- sleep 50s
-
+ sleep 30s
+ echo "executing cql script for cassandra to get the tables ready"
  docker-compose exec cassandra sh /scripts/initial-script.sh
+
+ echo "kicking off webservice to write data to mysql which ultimately will be replicated to cassandra."
+
+ sleep 10s
+ curl http://localhost:8080/demo/start
+
+ sleep 10s
+ curl http://localhost:8080/demo/stop
 
  exit
