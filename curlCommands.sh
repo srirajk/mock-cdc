736ac7cb0845bd8a76d42aa318fc1fb3507e1fa7
raj <raj@rajs-MacBook-Pro.local>
2017-07-28 14:16:46 -0400

    curl commands to add templates.

--- curlCommands.sh
+++ curlCommands.sh
@@ -3,7 +3,14 @@
 
 
 
- curl http://localhost:8080/demo/start
 
- sleep 10s
- curl http://localhost:8080/demo/stop
+curl  -d @./nifi/nifi-templates/mysql_-_kafka.xml -H 'Content-Type: application/xml' -X POST http://localhost:9000/nifi-api/process-groups/root/templates/import
+
+curl  -d @./nifi/nifi-templates/kafka_-_cassandra.xml -H 'Content-Type: application/xml' -X POST http://localhost:9000/nifi-api/process-groups/root/templates/import
+
+
+curl http://localhost:8080/demo/start
+
+sleep 10s
+
+curl http://localhost:8080/demo/stop
